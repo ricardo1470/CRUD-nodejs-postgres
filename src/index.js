93470bad9d15bfacc74059fb699bb6239fed0b3d
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2021-04-20 12:53:19 -0500

    add module http-errors
    add errors file
    
    confit server midelware errors

--- src/index.js
+++ src/index.js
@@ -9,6 +9,9 @@ var cors = require('cors');
 const morgan =  require("morgan");
 /* import path */
 const path = require('path');
+/* import http-errors */
+var createError = require('http-errors');
+
 /* port assignment */
 const port = 9000 || process.env.PORT;
 
@@ -32,6 +35,22 @@ app.use(require('./routes/index'))
 
 /* static files */
 app.use(express.static(path.join(__dirname, 'public')));
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    next(createError(404));
+});
+
+  // error handler
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
 
 /* listen server on port */
 app.listen(port, function () {
