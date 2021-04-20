efb302683037fe9b7031d8cb140dbab70cf094fd
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2021-04-20 12:58:57 -0500

    add module http-errors
    add errors file
    
    confit server midelware errors

--- src/index.js
+++ src/index.js
@@ -41,7 +41,7 @@ app.use(function(req, res, next) {
     next(createError(404));
 });
 
-  // error handler
+// error handler
 app.use(function(err, req, res, next) {
     // set locals, only providing error in development
     res.locals.message = err.message;
@@ -49,7 +49,7 @@ app.use(function(err, req, res, next) {
 
     // render the error page
     res.status(err.status || 500);
-    res.render('error');
+    res.render('error.html', { title: '@Ricardo1470' });
 });
 
 /* listen server on port */
